6dcbd3701066f68f7785b804d84af9b34ce3fbe2
TheBude <husaenbobmuratov192@gmail.com>
2024-04-19 03:51:59 +0500

    Add project files.

---
+++ task3_/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Butun Son kiriting: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if  (num % 2 == 0)
+{
+    Console.WriteLine("Kirtilgan son juf son!");
+}
+else
+{
+    Console.WriteLine("Kiritlgan son toq son!");
+}
