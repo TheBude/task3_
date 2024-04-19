589b292d40f872913bf58a7c87237c445346fa2d
TheBude <husaenbobmuratov192@gmail.com>
2024-04-19 05:29:59 +0500

    Xarajatni hisoblash

--- task3_/Program.cs
+++ task3_/Program.cs
@@ -1,20 +1,15 @@
-﻿//Darajaga oshuruvchi dastur
-Console.Write("Sonni kiriting: ");
-int son = Convert.ToInt32(Console.ReadLine());
-Console.Write("Darajani kiriting: ");
-int daraja = Convert.ToInt32(Console.ReadLine());
+﻿//Xarajatlarni hisoblash
+Console.WriteLine("Harajatlarni Hisoblavchi dastur!\nHAr bir harajatni vergul bilan ajratib yozing");
 
-if (daraja < 0)
-{
-    Console.WriteLine("Daraja Noldon katta Bo'lishi zarur");
-}
-else
-{
-    int natija = 1;
-    for (int i = 0; i < daraja; i++)
-    {
-        natija *= son;
-    }
+Console.Write("Xarajatlar Sonnini kirtiting: ");
+int massiv = int.Parse(Console.ReadLine());
+int[] count = new int[massiv];
+int num = 0;
 
-    Console.WriteLine($"Natija: {natija}");
+for (int i = 0; i < massiv; i++)
+{
+    Console.Write($"{i + 1} - Xarajatni kiriting: ");
+    count[i] = int.Parse(Console.ReadLine());
+    num += count[i];
 }
+Console.WriteLine(num + " So'm Xarajat bo'lgan");
